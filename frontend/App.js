931e45dfff8e101566d3e0afa3b2a7c40ee98991
Alexander Hetzmannseder <github@hetzmannseder.dev>
2023-04-27 11:33:10 +0200

    [TASK] header

--- frontend/App.js
+++ frontend/App.js
@@ -1,20 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import Header from './components/header';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { SafeAreaView, StyleSheet, Text } from "react-native";
+import Header from "./components/header";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Header/>
+    <SafeAreaView style={styles.container}>
+      <Header />
       <Text>Open up App.js to start working on your amazing app!</Text>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    marginTop: 20, // Adjust this value as needed to move the header down
+  },
+  header: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    height: 60,
+    backgroundColor: "#fff",
+    paddingHorizontal: 20,
+    borderBottomWidth: 1,
+    borderBottomColor: "#ccc",
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  icon: {
+    width: 24,
+    height: 24,
+    tintColor: "#007aff",
   },
 });
--- frontend/components/header.tsx
+++ frontend/components/header.tsx
@@ -1,23 +1,35 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Image, StyleSheet, Text, View } from "react-native";
 
 export default function Header() {
   return (
     <View style={styles.header}>
-      <Text style={styles.title}>Persona Generator</Text>
+      <Text style={styles.title}>Random Persona Generator</Text>
+      <Image
+        source={require('../assets/icon.png')}
+        style={styles.icon}
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   header: {
-    height: 80,
-    paddingTop: 38,
-    backgroundColor: "coral",
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    height: 60,
+    backgroundColor: '#fff',
+    paddingHorizontal: 20,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc',
   },
   title: {
-    textAlign: "center",
-    color: "#fff",
     fontSize: 20,
-    fontWeight: "bold",
+    fontWeight: 'bold',
+  },
+  icon: {
+    width: 24,
+    height: 24,
+    tintColor: '#007aff',
   },
 });
