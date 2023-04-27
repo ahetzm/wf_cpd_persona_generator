0aa14f2c8f3d2c4bcd7b60a8c18d228549d21d96
Alexander Hetzmannseder <github@hetzmannseder.dev>
2023-04-27 09:24:21 +0200

    formatting test commit

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
-import express from 'express';
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
+import express from "express";
 // import cors from 'cors';
-import bodyParser from 'body-parser';
-import morgan from 'morgan';
-import compression from 'compression';
-import { Configuration, OpenAIApi } from 'openai';
-import { errorHandling, errorLogger, invalidPathHandler, wrap } from './middleware/error.js';
-
+import bodyParser from "body-parser";
+import compression from "compression";
+import morgan from "morgan";
+import { Configuration, OpenAIApi } from "openai";
+import {
+  errorHandling,
+  errorLogger,
+  invalidPathHandler,
+  wrap,
+} from "./middleware/error.js";
 
 // Init Express
 const app = express();
-const morganLogFormat = ':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] ":user-agent"';
+const morganLogFormat =
+  ':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] ":user-agent"';
 app.use(morgan(morganLogFormat));
 // app.use(cors());
 app.use(compression());
@@ -19,25 +24,31 @@ app.use(bodyParser.json());
 dotenv.config();
 
 // Init OpenAI
-const openai = new OpenAIApi(new Configuration({
+const openai = new OpenAIApi(
+  new Configuration({
     apiKey: process.env.OPENAI_API_KEY,
-}));
+  })
+);
 
 // Routes
-app.get('/', wrap(async (req, res, next) => {
+app.get(
+  "/",
+  wrap(async (req, res, next) => {
     console.log(req.protocol + "://" + req.headers.host);
-    const routes = []
+    const routes = [];
     app._router.stack.forEach(function (r) {
-        if (r.route && r.route.path) {
-            routes.push({ path: r.route.path, method: r.route.methods });
-        }
-    })
-    return res.json({ message: 'Persona Generator Backend', routes: routes });
-}));
+      if (r.route && r.route.path) {
+        routes.push({ path: r.route.path, method: r.route.methods });
+      }
+    });
+    return res.json({ message: "Persona Generator Backend", routes: routes });
+  })
+);
 
-app.get('/api/persona', wrap(async (req, res, next) => {
-
-}));
+app.get(
+  "/api/persona",
+  wrap(async (req, res, next) => {})
+);
 
 // Error Handling
 app.use(errorLogger);
@@ -47,6 +58,9 @@ app.use(invalidPathHandler);
 // Start listening
 const port = process.env.PORT || 7000;
 app.listen(port, () => {
-    console.log('🧏 ' + new Date().toISOString() + ' - listening on port =', port);
-    console.log('--- Successfully Initialised NodeJS Backend ---');
+  console.log(
+    "🧏 " + new Date().toISOString() + " - listening on port =",
+    port
+  );
+  console.log("--- Successfully Initialised NodeJS Backend ---");
 });
