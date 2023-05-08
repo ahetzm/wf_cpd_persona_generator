3147c0cd36109548cb10a20619ec925d66a231a5
Jakob Osterberger <s2010456022@students.fh-hagenberg.at>
2023-05-08 21:26:23 +0200

    form styles

--- frontend/components/create-person-form.tsx
+++ frontend/components/create-person-form.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigation } from "@react-navigation/native";
-import { View, Text, TextInput, TouchableOpacity } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet} from 'react-native';
 import usePersonaService from "../services/persona-service";
 import { PersonaRequest } from '../models/PersonaInterface';
 import { Person } from '../models/Person';
@@ -55,49 +55,92 @@ const CreatePersonForm: React.FC<any> = () => {
   }
 
   return (
-    <View>
-      <Text>Fill out the form below to create a new persona</Text>
-      <Text>{loading}</Text>
-      <Text>Purpose/Context:</Text>
+    <View style={styles.container}>
+      {/* <Text style={styles.title}>Fill out the form below to create a new persona</Text> */}
+      {loading && <Text>Loading...</Text>}
+      <Text style={styles.inputLabel}>Purpose/Context:</Text>
       <TextInput
+        style={styles.input}
         multiline={true}
-        numberOfLines={4}
+        numberOfLines={2}
         value={purposeContext}
         onChangeText={text => setPurposeContext(text)}
       />
-      <Text>Name:</Text>
+      <Text style={styles.inputLabel}>Name:</Text>
       <TextInput
+        style={styles.input}
         value={name}
         onChangeText={text => setName(text)}
       />
-      <Text>Age:</Text>
+      <Text style={styles.inputLabel}>Age:</Text>
       <TextInput
+        style={styles.input}
         keyboardType="numeric"
         value={age}
         onChangeText={text => setAge(text)}
       />
-      <Text>Interests:</Text>
+      <Text style={styles.inputLabel}>Interests:</Text>
       <TextInput
+        style={styles.input}
         value={interests}
         onChangeText={text => setInterests(text)}
       />
-      <Text>Goals:</Text>
+      <Text style={styles.inputLabel}>Goals:</Text>
       <TextInput
+        style={styles.input}
         value={goals}
         onChangeText={text => setGoals(text)}
       />
-      <Text>Additional Info:</Text>
+      <Text style={styles.inputLabel}>Additional Info:</Text>
       <TextInput
-        multiline={true}
-        numberOfLines={4}
+        style={styles.input}
         value={additionalInfo}
         onChangeText={text => setAdditionalInfo(text)}
       />
-      <TouchableOpacity onPress={handleSubmit}>
-        <Text>Submit</Text>
+      <TouchableOpacity style={styles.button} onPress={handleSubmit}>
+        <Text style={styles.buttonText}>Submit</Text>
       </TouchableOpacity>
     </View>
   );
 };
 
 export default CreatePersonForm;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingHorizontal: 20,
+    backgroundColor: '#fff',
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  inputLabel: {
+    fontWeight: 'bold',
+    marginBottom: 5,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 5,
+    padding: 10,
+    marginBottom: 20,
+    width: '100%',
+    fontSize: 16,
+  },
+  button: {
+    backgroundColor: '#008080',
+    padding: 10,
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: '#fff',
+    fontWeight: 'bold',
+    textAlign: 'center',
+    fontSize: 16,
+  },
+});
